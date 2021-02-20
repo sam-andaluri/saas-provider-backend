c6c7e620f8c1f0b1742fbb80e537e439b725f237
Sambasiva Andaluri <andaluri@gmail.com>
2021-02-20 15:03:41 -0500

    misc fixes

--- app/main.py
+++ app/main.py
@@ -13,6 +13,9 @@ from nacl import encoding, public
 from pydantic import BaseModel
 from tinydb import TinyDB, where
 from github import Github
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, format=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') )
 
 # Tenant object received from provider UI
 class Tenant(BaseModel):
@@ -31,32 +34,32 @@ app = FastAPI()
 db = TinyDB('/data/tenant-db.json')
 tenants = db.table('tenants')
 
-# Load from secrets
-github_token = os.environ.get("GITHUB_TOKEN","1ff971d0dc076f4d74c2ad6692eee0e756ca1002")
-github_user = os.environ.get("GITHUB_USER","sam-andaluri")
-github_templated_repo = os.environ.get("GITHUB_TEMPLATED_REPO","saas-tenant-k8s-template")
-aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID", "AKIAWHAXMPNBZQU57THI")
-aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY", "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")
+# Load from kubernetes sealed secrets
+github_token = os.environ.get("GITHUB_TOKEN")
+github_user = os.environ.get("GITHUB_USER")
+github_templated_repo = os.environ.get("GITHUB_TEMPLATED_REPO")
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 # Create GitHub objects
 github = Github(github_token)
 github_user = github.get_user()
 
-# Encrypt secrets
-def encrypt(public_key: str, secret_value: str) -> str:
-    public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
-    sealed_box = public.SealedBox(public_key)
-    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
-    return b64encode(encrypted).decode("utf-8")
-
-# Get Public key from repo. Used for encrypting secrets
+# Get Public key from repo. Used for encrypting github secrets
 def gh_get_publickey(owner, repo, token):
     query_url = f"https://api.github.com/repos/{owner}/{repo}/actions/secrets/public-key"
     headers = {'Authorization': f'token {token}'}
     r = requests.get(query_url, headers=headers).json()
     return [r["key_id"], r["key"]]
 
-# Add secret to new tenant repo
+# Encrypt github secrets
+def encrypt(public_key: str, secret_value: str) -> str:
+    public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
+    sealed_box = public.SealedBox(public_key)
+    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+    return b64encode(encrypted).decode("utf-8")
+
+# Add github secrets to new tenant repo
 def gh_add_secret(owner, repo, token, secret_name, secret_value):
     public_key_tuple = gh_get_publickey(owner, repo, token)
     key_id = public_key_tuple[0]
@@ -73,16 +76,17 @@ def gh_add_secret(owner, repo, token, secret_name, secret_value):
         "key_id": key_id
     }
     r = requests.put(query_url, headers=headers, data=json.dumps(params))
-    pprint(r)
+    logging.debug(r)
 
-@app.get("/test")
-async def get_test():
+# Test url for testing front-end integration
+@app.get("/ping")
+async def get_pong():
     return json.dumps({"status":"success", "time": str(datetime.now())})
 
 # API for creating a tenant
 @app.post("/tenant/")
 async def create_tenant(tenant: Tenant):
-    print("got a new tenant request " + json.dumps(tenant) + " @ " + datetime.now())
+    logging.debug("create_tenant enter " + json.dumps(tenant))
     if tenant.namespace == None:
         tenant.namespace = pydng.generate_name()
     if tenant.created_time == None:
@@ -90,38 +94,51 @@ async def create_tenant(tenant: Tenant):
     if tenant.cloud_provider == None:
         tenant.cloud_provider = "AWS"
 
-    print("final tenant obj " + json.dumps(tenant))
+    logging.debug("create_tenant overrides " + json.dumps(tenant))
+
+    # Each tenant gets their own repo
     repo_name = "saas-tenant-" + tenant.namespace
 
-    print("creating new repo " + repo_name + " @ " + datetime.now())
+    # Create tenant repo
+    logging.debug("creating_tenant_repo " + repo_name)
     tenant_repo_obj = github_user.create_repo(repo_name)
 
-    print("adding repo secrets " + repo_name + " @ " + datetime.now())
+    # Add github secrets to repo
+    logging.debug("create_tenant add_secrets " + repo_name)
     gh_add_secret(github_user, repo_name, github_token, "AWS_ACCESS_KEY_ID", aws_access_key_id)
     gh_add_secret(github_user, repo_name, github_token, "AWS_SECRET_ACCESS_KEY", aws_secret_access_key)
 
-    print("creating files from templates" + " @ " + datetime.now())
+    # Access the tenant template repo
+    logging.debug("create_tenant copy_template")
     templated_repo_obj = github_user.get_repo(github_templated_repo)
+
+    # Create contents of tenant repo from template repo contents
+    logging.debug("create_tenant prepare_argo_app")
     argocd_app_spec_template = Template(templated_repo_obj.get_contents("/tier-customization/application.yaml").decoded_content.decode('ascii'))
     argocd_app_spec = argocd_app_spec_template.substitute(tenantId=tenant.namespace, repo="saas-tenant-" + tenant.namespace)
     kust_template = Template(templated_repo_obj.get_contents("/tier-customization/kustomization.yaml").decoded_content.decode('ascii'))
     kust_spec = kust_template.substitute(tenantId=tenant.namespace, tier=tenant.tier)
 
-    print("copying files to new repo" + " @ " + datetime.now())
+    logging.debug("create_tenant prepare_tenant")
     for repo_file in templated_repo_obj.get_contents("/tier"):
-        print(repo_file.decoded_content)
+        logging.debug("create_tenant file_contents" + repo_file.decoded_content)
         tenant_repo_obj.create_file("kubernetes/" + repo_file.name, "creating tenant", repo_file.decoded_content)
     tenant_repo_obj.create_file("kubernetes/kustomization.yaml", "creating tenant tier", kust_spec.encode('ascii'))
     gh_action_file = templated_repo_obj.get_contents("/tier-customization/deploy-argocd-app.yaml")
     tenant_repo_obj.create_file(".github/workflows/deploy.yaml", "adding deployment", gh_action_file)
     tenant_repo_obj.create_file("application.yaml", "creating tenant app", argocd_app_spec.encode('ascii'))
 
-    print("saving tenant to db" + " @ " + datetime.now())
+    # Create an ArgoCD app
+
+    # Sync ArgoCD app
+
+    # Save tenant info
+    logging.debug("create_tenant save_tenant")
     results = tenants.search(where("email")==tenant.email)
+    tenant.tenant_url = tenant.namespace + ".saas-tenant.cloud"
+    logging.debug("create_tenant dump_tenant_obj" + json.dumps(tenant))
     if results.len == 0:
         tenants.insert(tenant)
     else:
         tenant = results[0]
-    #TODO add background task to get tenant URL
-    #https://fastapi.tiangolo.com/tutorial/background-tasks/
     return tenant
